789c2f3387d607e19c89d302bfe07d5d3d5607e3
stefan <stefanfrankbio@gmail.com>
2022-05-03 11:42:32 +0200

    Added error count to EOF

--- run.sh
+++ run.sh
@@ -9,9 +9,9 @@ VR_DIR="${OUT_DIR}/variant_records"
 ERR_DIR="${OUT_DIR}/error_variant_records"
 
 
-#rm -r $ALIGN_DIR
-#rm -r $VR_DIR
-#rm -r $ERR_DIR
+rm -r $ALIGN_DIR
+rm -r $VR_DIR
+rm -r $ERR_DIR
 mkdir $ALIGN_DIR
 mkdir $VR_DIR
 mkdir $ERR_DIR
@@ -19,3 +19,5 @@ mkdir $ERR_DIR
 
 find $VARIANT_DIR -type f | xargs -n 1 --max-procs=$THREADS stretcher -bsequence $REFERENCE -adirectory3 $ALIGN_DIR -aformat fasta -auto
 find $ALIGN_DIR -type f | xargs --max-args=1 --replace=1 --max-procs=$THREADS python scripts/variant_record.py --alignment 1 --reference $REFERENCE --variants $VARIANT_DIR --error $ERR_DIR --output $VR_DIR
+
+ls $ERR_DIR | wc -l
